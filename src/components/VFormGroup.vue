8c43b2c066435c3c4e6458fb0f1c9800b9629d20
vovasap <vovasap.pblc@gmail.com>
2021-11-14 23:28:47 +0300

    feat: create form group

---
+++ src/components/VFormGroup.vue
@@ -0,0 +1,28 @@
+<template>
+  <fieldset class="group__fieldset">
+    <legend class="group__title">{{ title }}</legend>
+    <slot />
+  </fieldset>
+</template>
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null,
+    }
+  }
+}
+</script>
+<style>
+.group__fieldset {
+  margin-bottom: 10px;
+  padding: 0;
+  border: 0;
+}
+.group__title {
+  margin-bottom: 10px;
+  color: #575757;
+  font-weight: 700;
+}
+</style>
