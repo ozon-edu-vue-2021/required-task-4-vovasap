ec511705d75f424aba4d1e29f026eb8fb977b76d
vovasap <vovasap.pblc@gmail.com>
2021-11-14 23:32:17 +0300

    feat: add form for personal data

---
+++ src/components/PersonalDataForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <VFormGroup title="Личные данные">
+    <div class="personal-data__name">
+      <VInput v-model="personalData.lastName" label="Фамилия" type="text" />
+      <VInput v-model="personalData.firsName" label="Имя" type="text" />
+      <VInput v-model="personalData.patronymic" label="Отчество" type="text" />
+    </div>
+    <VInput v-model="personalData.birthDay" label="Дата рождения" type="date" />
+    <VInput v-model="personalData.email" label="E-mail" type="email" />
+    <VRadio
+      v-model="personalData.gender"
+      :options="genderOptions"
+      title="Пол"
+    />
+  </VFormGroup>
+</template>
+<script>
+import VInput from '@/components/VInput.vue'
+import VRadio from '@/components/VRadio.vue'
+import VFormGroup from '@/components/VFormGroup.vue'
+export default {
+  name: 'PersonalDataForm',
+  components: {
+    VInput,
+    VFormGroup,
+    VRadio,
+  },
+  props: {
+    value: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      genderOptions: [
+        { label: 'мужской', value: 'male' },
+        { label: 'женский', value: 'female' },
+      ],
+    }
+  },
+  computed: {
+    personalData: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      },
+    },
+  },
+}
+</script>
+<style>
+.personal-data__name {
+  display: flex;
+  gap: 5px;
+}
+</style>
